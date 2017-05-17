88c1b6ae79943905bb09da0441b242295b6c0794
My Name <myEmail@example.com>
2017-05-17 17:09:59 +0800

    add src

---
+++ src/src/com/garry/decorator/StudentFamilyInformationAdd.java
@@ -0,0 +1,10 @@
+package com.garry.decorator;
+
+/**
+ * Created by Administrator on 2017/5/17 0017.
+ */
+public class StudentFamilyInformationAdd extends StudentDecorator {
+    public void add(){
+        System.out.println("add studentFamilyInformation");
+    }
+}
---
+++ src/src/com/garry/decorator/test.java
@@ -0,0 +1,7 @@
+package com.garry.decorator;
+
+/**
+ * Created by Administrator on 2017/5/17 0017.
+ */
+public class test {
+}
